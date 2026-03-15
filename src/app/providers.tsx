3b3de358e0c56529726f5e95a871b4a793ac97af
MrYanis77 <yanislaldjipro@gmail.com>
2026-03-15 13:21:00 +0100

    version final après le 11 /03/2026

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,12 @@
 "use client"
+
 import {HeroUIProvider} from '@heroui/react'
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { AppProvider } from "../context/AppContext";
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
-     <NextThemesProvider attribute="class" defaultTheme="dark">
+     <NextThemesProvider attribute="class" defaultTheme="white">
       <HeroUIProvider>
         <AppProvider>
           {children}
